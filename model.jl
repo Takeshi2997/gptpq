239da6d4aa8181e74b70f09bd96a63087685152b
Takeshi2997 <takeshi6.6260@gmail.com>
2021-07-20 07:38:23 +0900

    fix

--- model.jl
+++ model.jl
@@ -2,39 +2,60 @@ include("./setup.jl")
 
 mutable struct State{T<:Real}
     spin::Vector{T}
-    shift::Vector{Vector{T}}
+#    shift::Vector{Vector{T}}
 end
-function State(x::Vector{T}) where {T<:Real}
-    shift = [circshift(x, s) for s in 1:c.NSpin]
-    State(x, shift)
-end
+# function State(x::Vector{T}) where {T<:Real}
+#    shift = [circshift(x, s) for s in 1:c.NSpin]
+#    State(x, shift)
+# end
 
 mutable struct GPmodel{T<:Complex}
     data_x::Vector{State}
     data_y::Vector{T}
+    data_z::Vector{State}
     pvec::Vector{T}
     KI::Array{T}
 end
 function GPmodel(data_x::Vector{State}, data_y::Vector{T}) where {T<:Complex}
-    KI = Array{T}(undef, c.NData, c.NData)
-    makeinverse(KI, data_x)
-    pvec = KI * exp.(data_y)
-    GPmodel(data_x, data_y, pvec, KI)
+    # Step 1
+    data_z = [State(rand([1f0, -1f0], c.NSpin)) for i in 1:c.Naux]
+    KMM = Array{T}(undef, c.Naux, c.Naux)
+    makematrix(KMM, data_z)
+    KMN = [kernel(data_z[i], data_x[j]) for i in 1:length(data_z), j in 1:length(data_x)]
+
+    # Step 2
+    Λ = Diagonal([kernel(data_x[i], data_x[i]) + KMN[:, i]' * (KMM \ KMN[:, i]) + 1f-6 for i in 1:length(data_x)])
+
+    # Step3
+    QMM = KMM + KMN * (Λ \ KMN')
+    u₀ = KMM * (QMM \ (KMN * (Λ \ ys)))
+    Σ₀ = KMM * (QMM \ KMM)
+    pvec = KMM \ u₀
+    KI = makeinverse(Σ₀)
+
+    # Output
+    GPmodel(xs, ys, zs, pvec, KI)
 end
 
 function kernel(x1::State, x2::State)
-    v = [norm(x1.shift[n] - x2.spin)^2 for n in 1:length(x1.spin)]
-    v ./= c.NSpin
-    sum(exp.(-v ./ c.A))
+#    v = [norm(x1.shift[n] - x2.spin)^2 for n in 1:length(x1.spin)]
+#    v ./= c.NSpin
+#    sum(exp.(-v ./ c.A))
+    v = norm(x1.spin - x2.spin)^2
+    v /= c.NSpin
+    exp(-v / c.A)
 end
 
-function makeinverse(KI::Array{T}, data_x::Vector{State}) where {T<:Complex}
+function makematrix(K::Array{T}, data_x::Vector{State}) where{T<:Complex}
     for i in 1:c.NData
         for j in i:c.NData
-            KI[i, j] = kernel(data_x[i], data_x[j])
-            KI[j, i] = KI[i, j]
+            K[i, j] = kernel(data_x[i], data_x[j])
+            K[j, i] = K[i, j]
         end
     end
+end 
+
+function makeinverse(KI::Array{T}) where {T<:Complex}
     # KI[:, :] = inv(KI)
     U, Δ, V = svd(KI)
     invΔ = Diagonal(1.0 ./ Δ .* (Δ .> 1e-6))
@@ -42,14 +63,16 @@ function makeinverse(KI::Array{T}, data_x::Vector{State}) where {T<:Complex}
 end
 
 function predict(x::State, model::GPmodel)
-    data_x, data_y, pvec, KI = model.data_x, model.data_y, model.pvec, model.KI
+    data_z, data_y, pvec, KI = model.data_z, model.data_y, model.pvec, model.KI
 
-    kv = [kernel(x, data_x[i]) for i in 1:c.NData]
+    # Compute mu var
+    kv = map(z -> kernel(z, x), data_z)
     k0 = kernel(x, x)
-    mu  = kv' * pvec
+    mu = kv' * pvec
     var = k0 - kv' * KI * kv
 
-    log(sqrt(var) * randn(typeof(mu)) + mu)
+    # sample from gaussian
+    log((exp(c.η * (sqrt(var) * randn(Complex{T}) + mu)) - 1.0) / c.η)
 end
 
 
--- setup.jl
+++ setup.jl
@@ -1,25 +1,29 @@
 struct GP_Data{T<:Real, S<:Integer}
     NSpin::S
     NData::S
+    Naux::S
     NMC::S
     MCSkip::S
     H::T
     t::T
     J::T
     l::T
+    η::T
     A::T
 end
 function GP_Data()
     NSpin = 80
-    NData = 64
-    NMC = 5120
+    NData = 1024
+    Naux = 64
+    NMC = 1024
     MCSkip = 16
     H = 2.0
     t = 1.0
     J = 1.0
     l = 1.6
+    η = 1e-3
     A = 0.21
-    GP_Data(NSpin, NData, NMC, MCSkip, H, t, J, l, A)
+    GP_Data(NSpin, NData, Naux, NMC, MCSkip, H, t, J, l, η, A)
 end
 c = GP_Data()
 
